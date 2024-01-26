6595fa308aa609b52ff001234163386d637ee70a
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-27 00:48:44 +0900

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,10 +1,8 @@
 #! /bin/bash
 
 python main.py \
-  --prompt "Black Leopard" \   # trg image prompt
-  --source "Lion" \            # source image prompt
-  --input_image "input_example/lion1.jpg" \  # input image
-  --output_path "../result/output_leopard" \
+  --prompt "Black Leopard" \
+  --source "Lion" --input_image "input_example/lion1.jpg" --output_path "../result/output_leopard" \
   --use_range_restart \
   --seed 42 \
   --use_noise_aug_all \
@@ -12,4 +10,4 @@ python main.py \
   --model_output_size 256 \
   --skip_timesteps 40 \
   --diff_iter 100 \
---iterations_num 10
+  --iterations_num 10
