fd43837bc2b23bc3a5e9d0e0a17f12921b717b86
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-06 00:11:38 +0900

    Update extractor.py

--- model_vit/extractor.py
+++ model_vit/extractor.py
@@ -18,8 +18,9 @@ class VitExtractor:
 
     def __init__(self, model_name, device):
         #self.model = torch.hub.load('facebookresearch/dino:main', model_name).to(device)
-        self.model = torch.load('/home/dreamyou070/DiffuseIT/checkpoints/dino_deitsmall8_pretrain_full_checkpoint.pth',
-                                ).to(device) # model_name
+        state_dict = torch.load('/home/dreamyou070/DiffuseIT/checkpoints/dino_deitsmall8_pretrain_full_checkpoint.pth',)#.to(device) # model_name
+        print(f'keys : {state_dict.keys()}')
+        self.model = torch.hub.load('facebookresearch/dino:main', model_name)
         self.model.eval()
         self.model_name = model_name
         self.hook_handlers = []
