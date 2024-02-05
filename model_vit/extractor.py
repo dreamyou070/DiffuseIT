c138271ef7eefe5f75b8b82dc1d93d8a96c6c38b
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-06 00:19:36 +0900

    a

--- model_vit/extractor.py
+++ model_vit/extractor.py
@@ -18,7 +18,8 @@ class VitExtractor:
 
     def __init__(self, model_name, device):
 
-        self.model = torch.hub.load('facebookresearch/dino:main', model_name).to(device)
+        self.model = torch.hub.load('facebookresearch/dino:main', model_name,
+                                    cache_dir = '../checkpoints').to(device)
         self.model.eval()
         self.model_name = model_name
         self.hook_handlers = []
